eb2fc6a82d4b05ee532e85895db1bc337d1dd2d6
kuThang <nqthang1306@gmail.com>
2019-10-14 22:48:25 +0900

    [w1] update solution

--- Percolation.java
+++ Percolation.java
@@ -3,146 +3,119 @@
  *  Date:
  *  Description:
  **************************************************************************** */
-public class Percolation {
-    private int ROW = 0;
-    private int COL = 0;
-    private int[][] id = null;
-    private int openSiteCount = 0;
 
-    // creates n-by-n grid, with all sites initially blocked
+public class Percolation {
+    private int RANGE = -1;
+    private Integer[] id;
+    private Integer[] sz;
+    private int countSites = 0;
+
     public Percolation(int n){
         if ( n <= 0 ) {
             throw new IllegalArgumentException();
         }
-        this.ROW = n;
-        this.COL = n;
-        this.id = new int[n][n];
-
-        for (int i = 0; i < n; i++){
-            for (int j=0; j < n; j++) {
-                this.id[i][j] = -1;
+        this.id = new Integer[n*n + 2];
+        this.sz = new Integer[n*n + 2];
+        this.RANGE = n;
+        for(int i = 0; i < n*n + 2; i++){
+            if(i == 0 || i == n*n + 1) {
+                this.id[i] = i;
+                this.sz[i] = 0;
+            } else {
+                this.id[i] = -1;
+                this.sz[i] = -1;
             }
         }
     }
 
-    private int root(int row, int col){
-        if (row < 1 || row > ROW || col < 1 || col > COL){
+    private boolean validCoordinate(int row, int col) {
+        return row >= 1 && row <= this.RANGE && col >= 1 && col <= this.RANGE;
+    }
+
+    private int toArrayIndex(int row, int col) {
+        return (row - 1) * this.RANGE + col;
+    }
+
+    private int root(int i){
+        if(id[i] == -1) {
             throw new IllegalArgumentException();
         }
-        while (id[row-1][col-1] != calculateId(row-1, col-1)) {
-            Integer [] coord = calculateCoor(id[row-1][col-1]);
-
-            row = coord[0] + 1;
-            col = coord[1] + 1;
+        while (id[i] != i) {
+            id[i] = id[id[i]];
+            i = id[i];
         }
-        return id[row-1][col-1];
+        return i;
     }
 
-    private void union(int row1, int col1, int row2, int col2) {
-        if(id[row2-1][col2-1] < 0){
+    private boolean connected(int x, int y){
+        return root(x) == root(y);
+    }
+
+    private void union(int x, int y) {
+        if (sz[x] < sz[y]) {
+            id[root(x)] = id[root(y)];
+        } else {
+            id[root(y)] = id[root(x)];
+        }
+        int sum = sz[x] + sz[y];
+        sz[x] = sum;
+        sz[y] = sum;
+    }
+
+    private void union(int row1, int col1, int row2, int col2){
+        if(!validCoordinate(row2, col2) || !isOpen(row2, col2)) {
             return;
         }
-        if (root(row1, col1) < root(row2, col2)){
-            System.out.println("root(row1, col1) = " + root(row1, col1) + " < root(row2, col2) = " + root(row2, col2));
-            Integer [] coord = calculateCoor(id[row2-1][col2-1]);
-            id[coord[0]][coord[1]] = root(row1, col1);
-            System.out.println("---- root(row1, col1) = " + root(row1, col1) + ", root(row2, col2) = " + root(row2, col2));
-        } else {
-            System.out.println("root(row1, col1) = " + root(row1, col1) + " >= root(row2, col2) = " + root(row2, col2));
-            Integer [] coord = calculateCoor(id[row1-1][col1-1]);
-            id[coord[0]][coord[1]] = root(row2, col2);
-            // id[row1-1][col1-1] = id[row2 -1][col2 -1];
-            System.out.println("---- root(row1, col1) = " + root(row1, col1) + ", root(row2, col2) = " + root(row2, col2));
-        }
-
-        for (int i = 1; i <= ROW; i++){
-            for (int j = 1; j <= COL; j++) {
-                if(this.id[i-1][j-1] > -1 ){
-                    System.out.println("** at ["+ i + "," + j+ "] value is " + this.id[i-1][j-1]);
-                }
-            }
-        }
-    }
-
-    private int calculateId(int row, int col){
-        int id = row * COL + col;
-        // System.out.println("-- id, col, row " + id + ", " + row + ", " + col);
-        return row * COL + col;
-    }
-
-    private Integer[] calculateCoor(int id) {
-        Integer[] coord = new Integer[2];
-        coord[0] = (int)(id / COL);
-        coord[1] = id % COL;
-        // System.out.println("- id, col, row " + id + ", " + coord[0] + ", " + coord[1]);
-        return coord;
+        union(toArrayIndex(row1, col1), toArrayIndex(row2, col2));
     }
 
     // opens the site (row, col) if it is not open already
-    public void open(int row, int col){
-        if (row < 1 || row > ROW || col < 1 || col > COL){
+    public void open(int row, int col) {
+        if (!validCoordinate(row, col)){
             throw new IllegalArgumentException();
         }
-        // calculateCoor(2);
-        // calculateCoor(50);
-        // calculateCoor(49);
-        //
-        // calculateId(0, 2);
-        // calculateId(5, 0);
-        // calculateId(2, 9);
-        if(!isOpen(row, col)){
-            // id[row-1][col -1] = row - 1;
-            id[row-1][col-1] = calculateId(row -1, col -1);
+        id[toArrayIndex(row, col)] = toArrayIndex(row, col);
+        sz[toArrayIndex(row, col)] = 1;
+        this.countSites += 1;
+        if(row == 1) {
+            union(0, toArrayIndex(row, col));
         }
-        if(row > 1) {
-            union(row, col, row - 1, col);
+        if(row == this.RANGE) {
+            union(toArrayIndex(row, col), this.RANGE * this.RANGE + 1);
         }
-        if(col > 1) {
-            union(row, col, row, col - 1);
-        }
-        if(row < ROW) {
-            union(row, col, row + 1, col);
-        }
-        if(col < COL) {
-            union(row, col, row, col + 1);
-        }
+        union(row, col, row - 1, col);
+        union(row, col, row, col - 1);
+        union(row, col, row + 1, col);
+        union(row, col, row, col + 1);
     }
 
     // is the site (row, col) open?
-    public boolean isOpen(int row, int col){
-        if (row < 1 || row > ROW || col < 1 || col > COL){
+    public boolean isOpen(int row, int col) {
+        if (!validCoordinate(row, col)){
             throw new IllegalArgumentException();
         }
-        return id[row-1][col-1] > -1;
+        return this.id[toArrayIndex(row, col)] > -1;
     }
 
     // is the site (row, col) full?
-    public boolean isFull(int row, int col){
-        if (row < 1 || row > ROW || col < 1 || col > COL){
+    public boolean isFull(int row, int col) {
+        if (!validCoordinate(row, col)){
             throw new IllegalArgumentException();
         }
-        return id[row-1][col-1] > -1 && root(row, col) < COL ;
-        // return id[row-1][col-1] < COL && id[row-1][col-1] > -1;
+        return isOpen(row, col) && connected(0, toArrayIndex(row, col));
     }
 
     // returns the number of open sites
-    public int numberOfOpenSites(){
-        return this.openSiteCount;
+    public int numberOfOpenSites() {
+        return countSites;
     }
 
     // does the system percolate?
-    public boolean percolates(){
-        for(int j = 1; j <= COL; j++) {
-            if(id[ROW-1][j-1] > -1 && root(ROW, j) < COL) {
-                return true;
-            }
-        }
-        return false;
+    public boolean percolates() {
+        return (this.RANGE > 0 && connected(0, this.RANGE * this.RANGE + 1));
     }
 
-    // test client (optional)
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
-
 }
